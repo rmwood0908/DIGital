6fc37edd2bb658e20dfee9fc47db42de7c6fc72f
trw286 <trw286@nau.edu>
2025-11-30 01:48:42 -0700

    Reapply "Merge branch 'account_prototype'"
    This reverts commit 9ddd3d4c54eaa33da49b8da58c7c233c9e1723cc.

--- Code/account/auth-api/server.js
+++ Code/account/auth-api/server.js
@@ -11,6 +11,8 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 4000;
+
 // signup method
 app.post('/api/auth/signup', async (req, res) => {
 
@@ -57,10 +59,15 @@ app.post('/api/auth/login', async (req, res) => {
     try {
         const { username, password } = req.body;
 
+        if( !username || !password ) {
+            return res.status(400).json({ ok: false,
+                                            error: 'Missing field(s).'});
+            }
+
         // sql query
         const query = `SELECT user_id, password_hash
                        FROM users
-                       WHERE username = $1`
+                       WHERE username = $1`;
 
         // send query to Postgres
         const { rows } = await pool.query(query, [username.trim()]);
