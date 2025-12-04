e31e024584244a00ac6e85f96b18654e9dc6e8f2
trw286 <trw286@nau.edu>
2025-12-03 22:47:20 -0700

    Readded .obj files, DragRotate.cs code
    Added .obj files for 3D model, wired all the materials and meshes up, and created the rotate logic.

---
+++ DIGital/Assets/Scripts/DragRotate.cs
@@ -0,0 +1,14 @@
+using Unity.Engine;
+
+// class
+public class DragRotate : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 100f;
+
+    // function
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotationSpeed * Mathf.Deg2Rad;
+        transform.Rotate(Vector3.up, -rotX, Space.World);
+    }
+}
