6c01b518c5631d6e5fd59c92e9c54620ea1c42d9
trw286 <trw286@nau.edu>
2025-12-04 17:47:18 -0700

    Installed brcyptjs

--- Code/account/auth-api/server.js
+++ Code/account/auth-api/server.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { pool } from './db.js';
 import artifactRoutes from './artifactRoutes.js';
 
